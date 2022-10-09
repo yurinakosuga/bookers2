213ff86781cf60708128d9ed1b782ad0350c97c2
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2022-10-09 13:23:20 +0000

    user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to user_path(@user.id), flash: { notice: 'You have updated book successfully.' } 
+    else
+      render :edit
+    end  
+  end
+  
   private
   # ストロングパラメータ
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get "/home/about" => "homes#about", as: "about"
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   
 end
